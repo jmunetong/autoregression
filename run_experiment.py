4bc2e73517e7aae76846bd13a084b85e7d86c2ed
jmuneton <muneton.juand@gmail.com>
2025-04-14 13:46:10 -0700

    successfully ran an experiment

--- run_experiment.py
+++ run_experiment.py
@@ -17,6 +17,7 @@ from data_preprocessing import preprocess_images,XrdDataset
 FEATURE_EXTRACTOR_PATH = "google/vit-base-patch16-224"
 DATA_PATH = "data"
 URL_MODEL = "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/blob/main/vae-ft-mse-840000-ema-pruned.safetensors"
+NUM_DETECTORS = 40
 
 def run(args):
     feature_extractor = ViTImageProcessor.from_pretrained(FEATURE_EXTRACTOR_PATH)
@@ -27,6 +28,7 @@ def run(args):
     vae = AutoencoderKL.from_single_file(URL_MODEL)
     vae.to(device)
     vae.train()
+    
 
     optimizer = optim.Adam(vae.parameters(), lr=1e-4)
     criterion = nn.MSELoss()
@@ -35,12 +37,18 @@ def run(args):
         for i, batch in enumerate(dataloader):
             if batch.ndim > 4:
                 batch = einops.rearrange(batch, 'd b c h w -> (d b) c h w')
-
-            batch = batch.to(device)
+            batch = einops.rearrange(batch, '(d b) c h w -> b d c h w', d=args.detectors_per_batch)
             optimizer.zero_grad()
-            recon_batch = vae(batch)
-            loss = criterion(recon_batch, batch)
-            loss.backward()
+
+            for j in range(batch.shape[0]):
+                print(f"Processing batch {j+1}/{batch.shape[0]}")
+                sub_batch = batch[j].to(device)
+                decoder_output = vae(sub_batch).sample
+                loss = criterion(decoder_output, sub_batch)
+                loss.backward()
+                optimizer.step()
+
+                
             optimizer.step()
             if i % 10 == 0:
                 print(f"Batch {i}, Loss: {loss.item()}")
@@ -55,6 +63,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--data_path", type=str, default=DATA_PATH, help="Path to the data directory")
     parser.add_argument("--batch_size", "-b", type=int, default=1, help="Batch size for training")
+    parser.add_argument("--detectors_per_batch", type=int, default=8, help="Number of detectors per batch")
     parser.add_argument("--num_epochs", "-e", type=int, default=10, help="Number of epochs for training")
     args = parser.parse_args()
     run(args)
