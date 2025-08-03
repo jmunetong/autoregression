3b9aa37cf09301ac68510b04bb9af369bd13796d
jmunetong <muneton.juand@gmail.com>
2025-08-03 08:38:06 -0700

    modifying arg parse parameters

--- run_experiment.py
+++ run_experiment.py
@@ -18,64 +18,64 @@ from data_preprocessing import XrdDataset
 accelerator = Accelerator(log_with="wandb")
 
 
+def print_main(accelerator, message, color="white"):
+    if accelerator.is_main_process:
+        print_color(message, color)
+
+
 def get_args():
     parser = ArgumentParser()
-
     os.makedirs("results", exist_ok=True)
-    # Model Name
-    parser.add_argument("--model_name", "-m", type=str, default="vae_kl", help="Name of model")
-    parser.add_argument("--latent_channels", type=int, default=4, help="Number of latent channels")
-    parser.add_argument("--train_vae", action="store_true", help="Train VAE model")
-    parser.add_argument("--inference", action="store_true", help="Run inference on the trained model")
-
-    # Data parameters
-    parser.add_argument("--data_id", type=int, default=522, choices=[422, 522], help="Experiment number")
-    parser.add_argument("--avg_pooling", action='store_true', help="Apply average pooling to the images")
-    parser.add_argument("--topk", type=float, default=1.0, help="Top k percent of images to use for training")
-
+    
     # Training parameters
-    parser.add_argument("--num_epochs", "-e", type=int, default=20, help="Number of epochs for training")
-    parser.add_argument("--lr", type=float, default=1e-4, help="learning rate training model")
-    parser.add_argument("--weight_decay", type=float, default=1e-3, help="Weight decay for optimizer")
-    parser.add_argument("--beta_recons", type=float, default=0.5, help="weight MSE Loss")
-    parser.add_argument("-recons_loss", "-rls", type=str, default="mse", choices=["mse", "l1", "iwmse"], help="Reconstruction loss type")
-    parser.add_argument("--alpha_mse", type=float, default=2.0, help="Alpha value for Intensity Weighted MSE Loss")
-
-    # Pipeline parameters
-    parser.add_argument("--data_path", type=str, default=DATA_PATH, help="Path to the data directory")
+    parser.add_argument("--model_name", "-m", type=str, default="vae_kl", help="Name of model")
     parser.add_argument("--batch_size", "-b", type=int, default=3, help="Batch size for training")
     parser.add_argument(
         "--test_pipeline", "-t",
         action="store_true",
         help="Enable test pipeline (default: False)"
     )
-
-    # VQ and VAE model parameters
-    parser.add_argument("--use_annealing", "-ua", action="store_true", help="Use annealing for KL divergence loss")
-    parser.add_argument("--annealing_shape", type=str, default="cosine", choices=["linear", "cosine", "logistic"], help="Shape of the annealing function")
-    parser.add_argument("--train_vae_from_checkpoint", action="store_true", help="Train VAE model from a checkpoint")
-    parser.add_argument("--train_vae_from_scratch", action="store_true", help="Train VAE model from scratch")
-    parser.add_argument("--pretrained_vae_path", type=str, default=None, help="Path to pretrained VAE model")
-
-
-    # Diffusion model arguments
-    parser.add_argument("--diff", action="store_true", help="Use diffusion model for training")
-    parser.add_argument("--latent_diffisuion", action="store_true", help="Use latent diffusion model")
-    parser.add_argument("--use_vae", action='store_true', help="Use VAE model for diffusion training")
-    parser.add_argument("--train_diff_from_checkpoint", action="store_true", help="Train diffusion model from a checkpoint")
-    parser.add_argument("--train_diff_from_scratch", action="store_true", help="Train diffusion model from scratch")
-    parser.add_argument("--pretrained_diff", type=str, default=None, help="Path to pretrained diffusion model")
-    parser.add_argument("--pretrained_vae", type=str, default=None, help="Path to pretrained VAE model") #TODO: REMOVE THIS ARGUMENT. Modify it in its correct place
-
-    parser.add_argument("--diff_epochs", type=int, default=10, help="Number of epochs for diffusion model training")
-    parser.add_argument("--patch_size", type=int, default=16, help="Patch size for diffusion model")
-    parser.add_argument("--vit_size", type=str, default="base", choices=["base", "large", "huge"], help="Size of the VIT model")
-    parser.add_arugment("--patch_size", type=int, default=16, help="Patch size for the VIT model")
+    parser.add_argument("--num_epochs", "-e", type=int, default=20, help="Number of epochs for training")
+    parser.add_argument("--lr", type=float, default=1e-4, help="learning rate training model")
+    parser.add_argument("--weight_decay", type=float, default=1e-3, help="Weight decay for optimizer")
+    parser.add_argument("--beta_recons", type=float, default=0.5, help="weight MSE Loss")
+    parser.add_argument("--recons_loss", "-rls", type=str, default="mse", choices=["mse", "l1", "iwmse"], help="Reconstruction loss type")
+    parser.add_argument("--alpha_mse", type=float, default=2.0, help="Alpha value for Intensity Weighted MSE Loss")
 
     # Inference parameters #TODO: Complete this part for running inference values
     parser.add_argument("--inference", action="store_true", help="Run inference on the trained model")
     parser.add_argument("--generate_samples", "-gs", action="store_true", help="Generate samples after training")
     parser.add_argument("--num_samples", type=int, default=10, help="Number of samples to generate")
+
+    # Data parameters
+    parser.add_argument("--data_id", type=int, default=522, choices=[422, 522], help="Experiment number")
+    parser.add_argument("--avg_pooling", action='store_true', help="Apply average pooling to the images")
+    parser.add_argument("--topk", type=float, default=1.0, help="Top k percent of images to use for training")
+    parser.add_argument("--data_path", type=str, default=DATA_PATH, help="Path to the data directory")
+    
+
+    # VQ and VAE model parameters
+    parser.add_argument("--latent_channels", type=int, default=4, help="Number of latent channels")
+    parser.add_argument("--use_annealing", "-ua", action="store_true", help="Use annealing for KL divergence loss")
+    parser.add_argument("--annealing_shape", type=str, default="cosine", choices=["linear", "cosine", "logistic"], help="Shape of the annealing function")
+    parser.add_argument("--train_vae_from_checkpoint", action="store_true", help="Train VAE model from a checkpoint")
+    parser.add_argument("--train_vae_from_scratch", action="store_true", help="Train VAE model from scratch")
+    parser.add_argument("--pretrained_vae_path", type=str, default=None, help="Path to pretrained VAE model")
+        # parser.add_argument("--train_vae", action="store_true", help="Train VAE model")
+
+    # Diffusion model arguments
+    parser.add_argument("--diff", action="store_true", help="Use diffusion model for training")
+    parser.add_argument("--latent_diffisuion", action="store_true", help="Use latent diffusion model")
+    # parser.add_argument("--use_vae", action='store_true', help="Use VAE model for diffusion training")
+    parser.add_argument("--train_diff_from_checkpoint", action="store_true", help="Train diffusion model from a checkpoint")
+    parser.add_argument("--train_diff_from_scratch", action="store_true", help="Train diffusion model from scratch")
+    parser.add_argument("--pretrained_diff_path", type=str, default=None, help="Path to pretrained diffusion model")
+    parser.add_argument("--diff_epochs", type=int, default=10, help="Number of epochs for diffusion model training")
+    parser.add_argument("--patch_size", type=int, default=16, help="Patch size for diffusion model")
+    parser.add_argument("--vit_size", type=str, default="base", choices=["base", "large", "huge"], help="Size of the VIT model")
+    parser.add_argument("--patch_size", type=int, default=16, help="Patch size for the VIT model")
+
+
     
     args = parser.parse_args()
     return args
@@ -103,30 +103,28 @@ def run(args):
     if args.train_diff_from_checkpoint and args.pretrained_diff_path is None:
         raise ValueError("Please provide a path to the pretrained diffusion model using --pretrained_diff")
     
+    
     if not args.diff or args.latent_diffisuion: 
         # Train a VAE or VQ model either for generative modeling or to train A vae model for latent diffusion.
-        print_color(f"Training {args.model_name} model", "blue")
+        print_main(accelerator, f"Running experiment {model_id} with model {args.model_name}", "blue")
         if args.model_name == "vae_kl":
              from train_utils.trainers import TrainerVAE as trainer
         else:
             from train_utils.trainers import TrainerVQ as trainer
         trainer_vae = trainer(args, accelerator,len_dataloader)
-        if args.train_vae_from_checkpoint or args.inference: #TODO: FIX THE INFERENCE PARAMETER
-            loading_directory = args.pretrained_vae
-            print_color(f"Loaded VAE model from {loading_directory if loading_directory else 'default path'}", "blue")
+        if args.train_vae_from_checkpoint or args.inference:
+            loading_directory = args.pretrained_vae_path
+            print_main(accelerator, f"Loading VAE model from {loading_directory if loading_directory else 'default path'}", "blue")
             trainer_vae.load_model(loading_directory)
 
         if (args.train_vae_from_scratch or args.train_vae_from_checkpoint) and not args.inference:
-            if accelerator.is_main_process:
-                print_color("Training VAE model", "blue")
+            directory = loading_directory if not args.pretrained_vae_path else directory
+            print_main(accelerator, f"Training VAE model with {len(dataloader)} batches", "blue")
             trainer_vae.run_train(dataloader, experiment_dict, directory)
 
         else:
-            if accelerator.is_main_process:
-                print_color("Skipping VAE training", "yellow")
-                print_color("Jumping to inference generation for VAE model", "yellow")
-          
-            # logging.info(f"Loaded model from {loading_directory if loading_directory else 'default path'}")
+            print_main(accelerator, "Skipping VAE training. Jumping to inference generation for VAE", "yellow")
+
         if accelerator.is_main_process:
             model_config = trainer_vae.get_model_config()
 
@@ -134,28 +132,25 @@ def run(args):
     if args.latent_diff:
         from train_utils.trainers import TrainerDiffusion as train_diff
         vae_model = trainer_vae.get_model(with_accelerator=False)
-        
         diffusion_trainer = train_diff(args, vae_model=vae_model, accelerator=accelerator,           input_shape=dataset.get_image_shape(), len_train_data_loader=len(dataloader))
         model_config = diffusion_trainer.get_model_config()
         
 
     if args.diff:
         from train_utils.trainers import TrainerDiffusionNonVAE as train_diff
-        if accelerator.is_main_process:
-            print_color("Training Diffusion model without VAE", "blue")
+        print_main(accelerator, f"Running diffusion model without VAE with {len(dataloader)} batches", "blue")
         diffusion_trainer = train_diff(args, accelerator=accelerator,           input_shape=dataset.get_image_shape(), len_train_data_loader=len(dataloader))
 
-        if accelerator.is_main_process:
-            print_color(f"Diffusion model shape: {diffusion_trainer.image_shape}", "blue")
+        print_main(accelerator, f"Diffusion model shape: {diffusion_trainer.image_shape}", "blue")
 
     if args.train_diff_from_checkpoint or args.inference:
-        loading_directory = args.pretrained_diff #TODO: ADD THIS ARGUMENT
-        if accelerator.is_main_process:
-            print_color(f"Loaded Diffusion model from {loading_directory if loading_directory else 'default path'}", "blue")
-    
+        loading_directory = args.pretrained_diff_path #TODO: ADD THIS ARGUMENT
+        print_main(accelerator, f"Loading Diffusion model from {loading_directory if loading_directory else 'default path'}", "blue")
+        diffusion_trainer.load_model(loading_directory)
+
     if (args.train_diff_from_checkpoint or args.train_diff_from_scratch) and not args.inference:
-        #todo: directory needs to change 
-        directory = loading_directory if not args.train_diff_from_scratch else directory
+        #todo: directory needs to change
+        directory = loading_directory if not args.pretrained_diff_path else directory
         diffusion_trainer.run_train(dataloader, experiment_dict, directory)
         if accelerator.is_main_process:
             model_config = diffusion_trainer.get_model_config()
@@ -189,20 +184,18 @@ def run(args):
     else:
         #TODO: MAKE THE INFERENCE GENERATION BE ACROSS EACH GPU.
         
-        if accelerator.is_main_process:
-            print_color(f"Generating {args.num_samples} samples with diffusion model", "blue")
+        print_main(accelerator, f"Generating {args.num_samples} samples with diffusion model", "blue")
         min_pixel, max_pixel = dataset.get_min_max()
         #TODO: MODIFY THIS FUNCTION FOR DIFFUSION WITHOUT VAE BACKEND.
 
-        generate_diff_samples(diffusion_trainer.get_model().eval(),  directory, idx_list=idx_list, encoding_shape=diffusion_trainer.encoding_shape, image_shape=diffusion_trainer.image_shape, min_pixel=min_pixel, max_pixel=max_pixel, use_vae=args.use_vae)
-
-        if args.use_vae:
-            print_color("Generating samples with VAE", "blue")
+        generate_diff_samples(diffusion_trainer.get_model().eval(),  directory, idx_list=idx_list, encoding_shape=diffusion_trainer.encoding_shape, image_shape=diffusion_trainer.image_shape, min_pixel=min_pixel, max_pixel=max_pixel, use_vae=args.latent_diff)
+ 
+        if args.latent_diff:
+            print_main(accelerator, "Generating samples with VAE", "blue")
             generate_vae_samples(trainer_vae.get_model().eval(), dataloader, directory, idx_list=idx_list)
         # generate_vae_samples(trainer_vae.get_model(), dataloader, directory)
 
 
-    
 
 
 if __name__ == '__main__':
